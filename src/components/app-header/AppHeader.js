36835304ff76c226ef00568f6efa1d07b5d3b56c
maxu20 <maxu20@jd.com>
2022-10-25 18:29:02 +0800

    header增加css

--- src/components/app-header/AppHeader.js
+++ src/components/app-header/AppHeader.js
@@ -1,16 +1,59 @@
-import {NavLink} from "react-router-dom";
 import "./AppHeader.css"
+import {NavLink} from "react-router-dom";
+
 
 function AppHeader() {
+
+    const headerLinks = [
+        {
+            title: '发现音乐',
+            link: '/',
+        },
+        {
+            title: '我的音乐',
+            link: '/myMusic',
+        },
+        {
+            title: '朋友',
+            link: '/follow',
+        },
+        {
+            title: '商城',
+            link: '/mall',
+        },
+        {
+            title: '音乐人',
+            link: '/musician',
+        },
+        {
+            title: '下载客户端',
+            link: '/download',
+        },
+    ]
+
+    const showSelectItem = (item, index) => {
+
+        return (
+            <NavLink
+                key={item.title}
+                to={item.link}
+                className={({isActive}) => isActive ? "app-header-item-active" : "app-header-item"}
+            >
+                <em>{item.title}</em>
+                <i className="app-header-item-active-icon"></i>
+            </NavLink>
+        );
+
+    };
+
     return (
         <div className="app-header-main">
-            <div className="app-header-logo"></div>
-            <NavLink to={"/"}>发现音乐 </NavLink>
-            <NavLink to={"/myMusic"}>我的音乐 </NavLink>
-            <NavLink to={"/follow"}>关注 </NavLink>
-            <NavLink to={"/mall"}>商城 </NavLink>
-            <NavLink to={"/musician"}>音乐人 </NavLink>
-            <NavLink to={"/download"}>下载客户端 </NavLink>
+            <div className="app-header-logo"/>
+            <div className="app-header-group">
+                {headerLinks.map((item, index) => {
+                    return showSelectItem(item, index);
+                })}
+            </div>
         </div>
     )
 }
