dca7337a3caa987ac9293b14f0048be9d98d0420
maxu20 <maxu20@jd.com>
2022-10-31 17:12:09 +0800

    引用图片资源

--- src/components/app-search/SearchContent.js
+++ src/components/app-search/SearchContent.js
@@ -1,7 +1,22 @@
 import "./SearchContent.css"
+
+import IconSingle from "../../assets/img/icon_single.png";
+
 function SearchContent() {
     return (
-        <div className="search-content"/>
+        <div className="search-content">
+            <div>搜"b"相关用户</div>
+            <div style={{height: 1, backgroundColor: "#ccc"}}/>
+            <div className="single-container">
+                <div className="single-title">
+                    <img src={IconSingle} alt='' width={15}/>
+                    <span>单曲</span>
+                </div>
+                <div className="single-content">
+
+                </div>
+            </div>
+        </div>
     )
 }
 
