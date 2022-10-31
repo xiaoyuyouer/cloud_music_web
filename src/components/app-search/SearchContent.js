ba3ee04a08991ecfc170f252da31adea050be729
maxu20 <maxu20@jd.com>
2022-10-31 17:53:29 +0800

    搜索组件基本layout

--- src/components/app-search/SearchContent.js
+++ src/components/app-search/SearchContent.js
@@ -1,11 +1,28 @@
 import "./SearchContent.css"
 
+import {RightOutlined} from "@ant-design/icons";
 import IconSingle from "../../assets/img/icon_single.png";
 
 function SearchContent() {
+
+    const data = [
+        '我-把灯墩柱',
+        'Japanese princess to wed commoner.',
+        'Australian walks 100km after outback crash.',
+        'Man charged over missing wedding girl.',
+        'Los Angeles battles huge wildfires.',
+    ];
+
+    const singleItems = data.map((myList) =>
+        <li>{myList}</li>
+    );
+
     return (
         <div className="search-content">
-            <div>搜"b"相关用户</div>
+            <div className="search-title">
+                搜"b"相关用户
+                <RightOutlined/>
+            </div>
             <div style={{height: 1, backgroundColor: "#ccc"}}/>
             <div className="single-container">
                 <div className="single-title">
@@ -13,7 +30,9 @@ function SearchContent() {
                     <span>单曲</span>
                 </div>
                 <div className="single-content">
-
+                    <ul>
+                        {singleItems}
+                    </ul>
                 </div>
             </div>
         </div>
